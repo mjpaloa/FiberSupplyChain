af3870aac2430c2619caa139e16741a12ab21fd8
mjpaloa <michaeljoshuabpaloa@adssu.edu.ph>
2026-07-14 14:33:05 +0800

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -38,7 +38,6 @@ const allowedOrigins = [
   'https://easyabaca.site',
   'https://www.easyabaca.site',
   'https://app.easyabaca.site',
-  'https://fibersupplychain.onrender.com',
   'http://localhost:5173',
   'http://localhost:3000'
 ];
